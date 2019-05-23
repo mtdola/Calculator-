862a0570fef967d421ab7ac34c10241f374d7a89
mtd <mtdolaj@gmail.com>
2019-05-23 10:14:10 +0100

    Addign JS first codes

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+var num = document.getElementsByClassName("numbers").value;
+var operator = document.getElementsByClassName("operators").value;
+//var a , b;
+//
+//function operation(operator) {
+//    if (operator = '+') {
+//        return a + b;
+//    }
+//    else if (operator == '-') {
+//        return a - b;
+//    }
+//    else if ( operator == '/') {
+//        retun a / b;
+//    }
+//    else if (operator == '*') {
+//        return a * b;
+//    }
+//}
+
 function calculate(num) {
-    
+    document.getElementById("display").innerHTML = num;
 }
