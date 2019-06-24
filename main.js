20d3da16d2625df694228f8bf227cbef920595df
mtd <mtdolaj@gmail.com>
2019-06-24 09:45:02 +0100

    made changes

--- main.js
+++ main.js
@@ -1,22 +1,7 @@
-var num = document.getElementsByClassName("numbers").value;
-var operator = document.getElementsByClassName("operators").value;
-//var a , b;
-//
-//function operation(operator) {
-//    if (operator = '+') {
-//        return a + b;
-//    }
-//    else if (operator == '-') {
-//        return a - b;
-//    }
-//    else if ( operator == '/') {
-//        retun a / b;
-//    }
-//    else if (operator == '*') {
-//        return a * b;
-//    }
-//}
-
-function calculate(num) {
-    document.getElementById("display").innerHTML = num;
+function getHistory() {
+    return document.getElementById("historyValue").innerText;
 }
+function prinHistory(num) {
+    
+}
+alert(getHistory());
