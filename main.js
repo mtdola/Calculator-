11bec6f0b9161d667bf1223d413ca6e3df914da2
Murtadore <mtdolaj@gmail.com>
2019-08-05 15:13:44 +0100

    started brainwork on the Js

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-function getHistory() {
-    return document.getElementById("historyValue").innerHTML;
-}
-function prinHistory(num) {
-	document.getElementById("historyValue").innerHTML=num;
+// function getHistory() {
+//     return document.getElementById("historyValue").innerHTML;
+// }
+// function prinHistory(num) {
+// 	document.getElementById("historyValue").innerHTML=num;
 	
-}
-printHistory(10);
+// }
+// printHistory(10);
 
+var historyValue = document.querySelector("#historyValue");
+console.log(historyValue);
